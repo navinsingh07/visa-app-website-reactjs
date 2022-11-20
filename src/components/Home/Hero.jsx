c23dd7b2de9ed5ff9119a8b4bc69f2e43d6b1864
navinsingh07 <neginavin99@gmail.com>
2022-11-20 12:20:59 +0530

    home page changes

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './style/HeroStyles.css'
-import img1 from '../../assets/images/img14.jpg'
+// import img1 from '../../assets/images/img14.jpg'
+import video from '../../assets/heroVideo.mp4'
 
 const Hero = () => {
   return (
     <>
       <div className="hero">
-        <img alt='heroImg' src={img1}/>
+        {/* <img alt='heroImg' src={img1}/> */}
+        <video  muted  autoPlay loop>
+          <source src={video} />
+        </video>
         <div className="hero-text">
         <h1>Apply Visa In A Simple Way </h1>
         <p>Be Ready To Fly</p>
